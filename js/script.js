5db10fd07debca5144e4ce054634203e4fae03d0
Jean Macedo <jfmacedo91@gmail.com>
2023-01-27 06:22:17 -0300

    add dynamic selection in budget

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const links = document.querySelectorAll(".header-menu a")
 
 links.forEach(link => {
-  const url = location.href
-  const href = link.href
+  const pageUrl = location.href
+  const linkUrl = link.href
 
-  if(url.includes(href)) {
+  if(pageUrl.includes(linkUrl)) {
     link.classList.add("active")
+  }
+})
+
+const params = new URLSearchParams(location.search)
+
+params.forEach(param => {
+  const checkbox = document.getElementById(param)
+  if(checkbox) {
+    checkbox.checked = true
   }
 })
